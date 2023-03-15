f8845f398fdba2d1291f3703ed417aa1fb7e689b
alinaduca <alina_duca@yahoo.com>
2023-03-15 12:02:07 +0200

    Lab4

--- Lab4/src/main/java/org/example/Main.java
+++ Lab4/src/main/java/org/example/Main.java
@@ -5,19 +5,27 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
+        //Creez o lista cu proiecte folosind stream.
         List<Project> projects = IntStream.rangeClosed(0, 2)
                 .mapToObj(i -> new Project("P" + i) )
                 .toList();
+
+        //Creez o lista cu studentii folosind stream.
         List<Student> students = IntStream.rangeClosed(0, 2)
                 .mapToObj(i -> new Student("S" + i, getProjects(projects, 3-i)))
                 .toList();
+
+        //Creez un obiect de tip LinkedList pentru studenti pentru a-i sorta ulterior, printr-un apel al metodei Collections.sort.
         List<Student> sortedStudents = new LinkedList<>(students);
         Collections.sort(sortedStudents);
         sortedStudents.forEach(System.out::println);
+
+        //Pun toate proiectele intr-un obiect de tipul TreeSet pentru a nu le sorta explicit.
         Set<Project> sortedProjects = new TreeSet<>(projects);
         sortedProjects.forEach(System.out::println);
     }
 
+    //Implementez o metoda care returneaza primele i proiecte (pentru a-mi furniza o lista cu proiectele de la 0 la 2-i).
     public static List<Project> getProjects(List<Project> projects, int index) {
         List<Project> newList = new ArrayList<>();
         int i = 0;
