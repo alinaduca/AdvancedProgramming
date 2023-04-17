36ed67c5dd97f4210b2fcfbab36d298264f33000
alinaduca <alina_duca@yahoo.com>
2023-04-17 09:15:18 +0300

    Paste fericit!

---
+++ Bonus4/src/main/java/org/example/Student.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.*;
+
+public class Student implements Node, Comparable<Student> {
+
+    private String name;
+//    private List<Project> admissibleProjects;
+
+    public Student(String name) {
+        this.name = name;
+//        this.admissibleProjects = admissibleProjects;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+//    public List<Project> getAdmissibleProjects() {
+//        return this.admissibleProjects;
+//    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return this.name/* + " admissible projects: " + this.admissibleProjects*/;
+    }
+}
